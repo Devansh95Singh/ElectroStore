12de9bd5a9b626875007bbafb3bd114f971d8715
devansh <devansh.singh.1.2.3@gmail.com>
2020-11-18 00:03:57 +0530

    Registering the routes

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from 'express' 
 import dotenv from 'dotenv'
-import products from './data/products.js'
 import connectDb from './config/db.js'
 import colors from 'colors'
+import { notFound, errorHandler} from './middleware/errorMiddleware.js'
+import productRoutes from './routes/productRoutes.js'
 
 connectDb()
 const app = express();
@@ -10,13 +11,9 @@ dotenv.config()
 app.get('/',(req, res)=>{
     res.send('Api is running....');
 })
-app.get('/api/product',(req, res)=>{
-    res.json(products);
-})
-app.get('/api/product/:id',(req, res)=>{
-    const product = products.find( (p) => p._id === req.params.id)
-    res.json(product);
-})
+app.use('/api/products', productRoutes)
+app.use(notFound)
+app.use(errorHandler)
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} at ${PORT}`.grey));
 
