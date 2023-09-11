e788bc98e50f1a769f111e0ec8bdc708850c1fe1
devansh singh <devansh.singh.1.2.3@gmail.com>
2023-09-12 00:17:18 +0530

    Add register user route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,19 @@
-import { loginUser, getUserProfile } from "../controllers/userController.js";
-import express from 'express';
-import { allowedForAdmin, tokenVerifier } from "../middleware/authMiddleware.js";
+import {
+  loginUser,
+  getUserProfile,
+  logout,
+  register,
+} from "../controllers/userController.js";
+import express from "express";
+import {
+  allowedForAdmin,
+  tokenVerifier,
+} from "../middleware/authMiddleware.js";
 
 const router = express.Router();
-router.post('/login', loginUser);
-router.route('/profile').get(tokenVerifier, allowedForAdmin, getUserProfile);
+router.post("/login", loginUser);
+router.route("/profile").get(tokenVerifier, allowedForAdmin, getUserProfile);
+router.route("/logout").get(tokenVerifier, logout);
+router.route("/register").post(register);
 
 export default router;
