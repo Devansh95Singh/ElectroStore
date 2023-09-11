34387ed0813c2a1eccd15c42844816373c88e955
devansh singh <devansh.singh.1.2.3@gmail.com>
2023-09-11 22:57:49 +0530

    Add authentication middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
@@ -8,14 +8,16 @@ const loginUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
-    const token = jwt.sign({ userId: user._id }, 'abc123', { expiresIn: '30d'});
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_TOKEN, {
+      expiresIn: "30d",
+    });
 
-    res.cookie('jwt', token, {
-        httpOnly: true,
-        secure: false,
-        sameSite: 'strict',
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-    })
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "strict",
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    });
     res.json({
       _id: user._id,
       email: user.email,
@@ -26,8 +28,10 @@ const loginUser = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Invalid user email or password");
   }
-
-  
 });
 
-export { loginUser };
+const getUserProfile = asyncHandler((req, res) => {
+    res.send('profile route')
+})
+
+export { loginUser, getUserProfile };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,9 @@
-import { loginUser } from "../controllers/userController.js";
+import { loginUser, getUserProfile } from "../controllers/userController.js";
 import express from 'express';
+import { allowedForAdmin, tokenVerifier } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 router.post('/login', loginUser);
+router.route('/profile').get(tokenVerifier, allowedForAdmin, getUserProfile);
 
 export default router;
