07f40c932d2452b6e3bb65a189df05e10b30b4eb
devansh singh <devansh.singh.1.2.3@gmail.com>
2023-09-11 22:14:58 +0530

    Add user login routes and add to cart frontend functionality

--- frontend/src/APISlices/cartSlice.js
+++ frontend/src/APISlices/cartSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { updateCart } from "../utils/cartUtils";
 
 const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
@@ -9,25 +10,23 @@ const cartSlice = createSlice({
   reducers: {
     addToCart: (state, action) => {
       const item = action.payload;
-      const existItem = state.cartItems.find(i => i._id === item._id);
+      const existItem = state.cartItems.find((i) => i._id === item._id);
       if (existItem) {
-        state.cartItems = state.cartItems.map((x) => x._id === existItem._id ? item : x)
+        state.cartItems = state.cartItems.map((x) =>
+          x._id === existItem._id ? item : x
+        );
       } else {
         state.cartItems = [...state.cartItems, item];
       }
 
-      state.itemsPrice = state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0);
-
-      state.shippingPrice = state.itemsPrice > 100 ? 10 : 0;
-
-      state.taxPrice = state.itemsPrice * 0.15;
-
-      state.totalPrice = (Number(state.itemsPrice) + Number(state.shippingPrice) + Number(state.taxPrice)).toFixed(2);
-
-      localStorage.setItem('cart', state);
-    }
+      return updateCart(state);
+    },
+    removeFromCart: (state, action) => {
+      state.cartItems = state.cartItems.filter((x) => x._id !== action.payload);
+      return updateCart(state);
+    },
   },
 });
 
-export const { addToCart } = cartSlice.actions; 
+export const { addToCart, removeFromCart } = cartSlice.actions;
 export default cartSlice.reducer;
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col, ListGroup, Card, Image, Alert, ListGroupItem, FormControl, Button} from 'react-bootstrap'
-import { addToCart, removeFromCart } from '../actions/cartActions'
+import { addToCart, removeFromCart } from '../APISlices/cartSlice';
 
 
 const CartScreen = ({ match, location, history }) => {
@@ -12,15 +12,13 @@ const CartScreen = ({ match, location, history }) => {
     const cart = useSelector(state => state.cart)
     const { cartItems } = cart
     
-    useEffect(()=>{
-        if(productId)
-        {
-            dispatch(addToCart(productId, qty))    
-        }
-    }, [dispatch, productId, qty])
-    const removeFromCartHandler = (id) => {
-      dispatch(removeFromCart(id))
-    }
+    const addToCartHandler = (product, qty) => {
+        dispatch(addToCart({ ...product, qty }));
+      };
+      const removeFromCartHandler = (product) => {
+        console.log(product);
+        dispatch(removeFromCart(product));
+      };
     const checkOutHandler = () => {
         history.push('/login?redirect=shipping')
     }
@@ -44,15 +42,14 @@ const CartScreen = ({ match, location, history }) => {
                                         ${item.price}
                                 </Col>
                                 <Col md={2}>
-                                    <FormControl as='select' value={item.qty} onChange={(e) => dispatch(addToCart(item.product,
-                                        Number(e.target.value)))}>
+                                    <FormControl as='select' value={item.qty} onChange={(e) => addToCartHandler(item, e.target.value)}>
                                     {[...Array(item.countInStock).keys()].map((x) => (
                                         <option key={x + 1} value={ x + 1}>{x + 1}</option>
                                     ))}
                                     </FormControl>
                                 </Col>
                                 <Col md={2}>
-                                    <Button type='button' variant='light' onClick={()=>{removeFromCartHandler(item.product)}}>
+                                    <Button type='button' variant='light' onClick={()=>{removeFromCartHandler(item._id)}}>
                                     <i className='fas fa-trash'></i>
                                     </Button>
                                 </Col>
